b2ce8fda09fd57adf2a10ad7c68183f50e0969f0
Jason <chisanch@usc.edu>
2024-09-24 20:41:18 -0400

    testing

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
-def main():
-    print("Hello from src!")
+from dotenv import load_dotenv
+import os
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
 
+"""
+TODO:
+- setup modal inference server
+"""
 
-if __name__ == "__main__":
-    main()
+load_dotenv()
+
+hf_access_token = os.getenv("HF_ACCESS_TOKEN")
+
+tokenizer = AutoTokenizer.from_pretrained(
+    "google/gemma-2-2b-it",
+    token=hf_access_token,
+)
+model = AutoModelForCausalLM.from_pretrained(
+    "google/gemma-2-2b-it",
+    device_map="auto",
+    output_hidden_states=True,
+    torch_dtype=torch.bfloat16,
+    token=hf_access_token,
+)
+
+input_text = "Write me a poem about Machine Learning."
+input_ids = tokenizer(input_text, return_tensors="pt").to("cuda")
+
+outputs = model.generate(**input_ids, max_new_tokens=32)
+print(tokenizer.decode(outputs[0]))
